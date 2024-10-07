5c8c338a5c63f8b162959bab3bdc99152bf82cc6
rahatbhambri97 <rahat.bhambri@alpha-grep.com>
2024-10-08 00:13:04 +0530

    fix: minor bugs

--- src/main.py
+++ src/main.py
@@ -31,26 +31,7 @@ def main():
     TicketGen = TicketGenerator(c_id)
     quantity = int(input("enter number of people"))
     TicketGen.BookTickets(user_id, m_id, quantity)
-                                                       
-                                                            
-    FaceRec.login_via_face()
-    user_id = FaceRec.user_id
-    
-    ip = input("Retrieve all bookings y/n ?")
-    if ip in ["y", "Y"]:
-        get_all_bookings(user_id=user_id)
-    
-    cinemas = get_all_cinemas()
-    for i, c in enumerate(cinemas):
-        print(i+1, c.get("name"))
-    id = int(input("enter cinema id to proceed "))
-    c_id = cinemas[id-1].get("cinema_id") 
-    
-    
-    TicketGen = TicketGenerator(c_id)
-    quantity = int(input("enter number of people"))
-    TicketGen.BookTickets(user_id, 1234, quantity)
-                                                       
+                                             
                                                             
 if __name__ == "__main__":
     main()
