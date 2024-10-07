8878194607f90a17816f1f9674ddf76a5b7358e4
rahatbhambri97 <rahat.bhambri@alpha-grep.com>
2024-10-08 00:08:55 +0530

    added changes for movie name

--- src/main.py
+++ src/main.py
@@ -3,8 +3,31 @@ from facial_entry import face_recon
 
 def main():
     FaceRec = face_recon.FaceRecognizer()
-    FaceRec.start_capture()
-    pass
-
+    FaceRec.login_via_face()
+    user_id = FaceRec.user_id
+    
+    ip = input("Retrieve all bookings y/n ?")
+    if ip in ["y", "Y"]:
+        get_all_bookings(user_id=user_id)
+    
+    cinemas = get_all_cinemas()
+    for i, c in enumerate(cinemas):
+        print(i+1, c.get("name"))
+    id = int(input("enter cinema id to proceed "))
+    c_id = cinemas[id-1].get("cinema_id") 
+    
+    print("Movies available at your selected cinema:- ")
+    movies = get_all_movies()
+    for i, m in enumerate(movies):
+        print(i+1, m.get("name"))
+    id = int(input("enter movie id to proceed "))
+    m_id = movies[id-1].get("movie_id") 
+    
+    
+    TicketGen = TicketGenerator(c_id)
+    quantity = int(input("enter number of people"))
+    TicketGen.BookTickets(user_id, m_id, quantity)
+                                                       
+                                                            
 if __name__ == "__main__":
     main()
---
+++ src/tickets/ticket.py
@@ -0,0 +1,24 @@
+from utils.utils import generate_random_string
+from database.db import get_connection
+from datetime import datetime
+
+
+class TicketGenerator:
+    def __init__(self, cinema_id):
+        self.cinema_id = cinema_id
+        self.db = get_connection()
+        
+    
+    def getTicketId(self):
+        return self.ticket_id
+    
+    def BookTickets(self, user_id, movie_id, quan): 
+        documents= []
+        for q in range(quan):
+            ticket_id = generate_random_string(8)
+            documents.append({"ticket_id": ticket_id, "movie_id" : movie_id, 
+                                        "user_id": str(user_id), "booking_time": datetime.now()})
+            print("booked ticket", ticket_id, "for user")
+        
+        self.db.tickets.insert_many(documents=documents)
+    
